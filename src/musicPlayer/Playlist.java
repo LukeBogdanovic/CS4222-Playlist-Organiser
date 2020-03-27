53e43817d9066454ed46ff8698608274917c6533
LukeBogdanovic <58079356+LukeBogdanovic@users.noreply.github.com>
2020-03-27 23:08:15 +0000

    version0.0.0

--- src/musicPlayer/Playlist.java
+++ src/musicPlayer/Playlist.java
@@ -1,5 +1,53 @@
 package musicPlayer;
+import java.util.*;
 
 public class Playlist {
+	private String Playlist;
+	private List<Track> Tracks;
+	
+	public Playlist() {
+		
+	}
+	
+	public String toString() {
+	String playlist,name;
+	name  = Playlist;
+	for(int i = 0;i < Tracks.size(); i++) {
+		Tracks.get(i).toString();
+	}
+	return playlist;
+	}
+	
+	public void add(String title,String artist) {
+		Track("Honey","BROCKHAMPTON",2018,195);
+	}
+	
+	public void add(String title,String artist,int year,int duration) {
+		
+	}
+	
+	public void add(Track t) {
+		
+	}
+	
+	public boolean remove(String title) {
+		if()
+	}
+	
+	public void showList() {
+		
+	}
+	
+	public void play(boolean random) {
+		
+	}
+	
+	public void playOnly(String artist) {
+		
+	}
+	
+	public void playOnly(int year) {
+		
+	}
 
 }
--- src/musicPlayer/Track.java
+++ src/musicPlayer/Track.java
@@ -1,5 +1,49 @@
 package musicPlayer;
+import java.util.*;
 
 public class Track {
+	private String Title;
+	private String Artist;
+	private int Year;
+	private int Duration;
+	List<String> songs = new ArrayList<String>();
+	
+	public Track(String title,String artist) {
+		Duration = 0;
+		Year = 0;
+		Artist = artist;
+		Title = title;
+		String track = toString();
+		songs.add(track);
+	}
+	
+	public Track(String title,String artist,int year,int duration) {
+		Duration = duration;
+		Year = year;
+		Artist = artist;
+		Title = title;
+		String track = toString();
+		songs.add(track);
+	}
+	
+	public String toString() {
+		String listing,trackTime;
+		int minutes,seconds;
+		minutes = Duration / 60;
+		seconds = Duration % 60;
+		trackTime = minutes + ":" + seconds;
+		if(Year == 0 && Duration == 0) {
+			listing = "Title:" + Title + "\n" + "Artist:" +  Artist;
+		}else {
+			listing = "Title:" + Title + "Released:" + Year + "\n" + "Artist:" +  Artist + "\n" + "Duration:" + trackTime;
+		}
+		return listing;
+	}
+	
+	public List<String> compareTo(List<String> music) {
+		List<String> Tracks = new ArrayList<String>(music);
+		Collections.sort(Tracks, String.CASE_INSENSITIVE_ORDER);
+		return Tracks;
+	}
 
 }
